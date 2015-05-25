983376b06659390c3e9f4fb0a2d75e54ddff4692
Luke Lu <popwar.lue@gmail.com>
2015-05-25 15:22:36 +1000

    first commit

---
+++ src/main/java/org/lu/FrontEndApp_Parent/App.java
@@ -0,0 +1,13 @@
+package org.lu.FrontEndApp_Parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
